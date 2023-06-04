531d975b8c2eb47d7a14fcb1232c50b27d27f3c8
Lars Vilhuber <lars.vilhuber@cornell.edu>
2023-06-04 20:51:12 +0000

    Updated code

--- config.R
+++ config.R
@@ -4,9 +4,9 @@
 
 issns.file <- file.path(dataloc,paste0("issns.Rds"))
 
-full.file <- file.path(dataloc,"crossrefdois")
-full.file.Rds <- paste(full.file,"Rds",sep=".")
-full.file.csv <- paste(full.file,"csv",sep=".")
+full.file.stem <- file.path(dataloc,"crossrefdois")
+full.file.Rds <- paste(full.file.stem,"Rds",sep=".")
+full.file.csv <- paste(full.file.stem,"csv",sep=".")
 
 new.file.Rds <- file.path(interwrk,paste0("new.Rds"))
 
@@ -18,3 +18,6 @@ Sys.setenv(crossref_email = "ldi@cornell.edu")
 # Entity we are searching for:
 
 affiliation.target = "World Bank"
+target.file.Rds = file.path(dataloc,"targets.Rds")
+target.file.csv = file.path(dataloc,"targets.csv")
+target.articles.csv = file.path(dataloc,"target-articles.csv")
